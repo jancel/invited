b148f28b5d207c523b0cd64b93827d0b1e60734b
Shams Quayyum <shamsq@gmail.com>
2011-11-16 21:28:51 -0600

    Created Device Registration and Session Creation activities

--- clients/android/src/com/invited/InvitedApplication.java
+++ clients/android/src/com/invited/InvitedApplication.java
@@ -1,7 +1,9 @@
 package com.invited;
 
+import android.provider.Settings.Secure;
+
 import com.github.droidfu.DroidFuApplication;
 
 public class InvitedApplication extends DroidFuApplication {
-
+	public static String appToken = "5742a8d8cba21cd6a19acda585642c94";
 }
